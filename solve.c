bb6d5997221f1071dce633ab070031ab571d35c0
Luca Rinelli <rinelliluca@gmail.com>
2018-03-11 17:55:14 +0100

    Files for solve + datastructure

--- solve.c
+++ solve.c
@@ -3,3 +3,4 @@
 //
 
 #include "solve.h"
+
--- solve.h
+++ solve.h
@@ -5,4 +5,8 @@
 #ifndef REPLY_TRAINING_CHALLENGE1_SOLVE_H
 #define REPLY_TRAINING_CHALLENGE1_SOLVE_H
 
+#include "datastructure.h"
+
+path_t *solve(field_t intervals_graphs);
+
 #endif //REPLY_TRAINING_CHALLENGE1_SOLVE_H
